21f39590bc2018c54989df84de0b3ecbc8b2adeb
Vagner Bom Jesus <vagneripg@gmail.com>
2026-02-06 13:05:36 +0000

    refactor: Change package name to com.bjbank.ipg for publishing
    - Updated Android namespace and applicationId in build.gradle.kts
    - Moved MainActivity.kt to new package structure
    - Updated iOS bundle identifier in project.pbxproj
    - Updated google-services.json package_name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.bjbank.ipg.bjbank"
+    namespace = "com.bjbank.ipg"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.bjbank.ipg.bjbank"
+        applicationId = "com.bjbank.ipg"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
--- android/app/src/main/kotlin/com/bjbank/ipg/MainActivity.kt
+++ android/app/src/main/kotlin/com/bjbank/ipg/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bjbank.ipg.bjbank
+package com.bjbank.ipg
 
 import io.flutter.embedding.android.FlutterActivity
 
